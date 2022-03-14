1b0b5872242e87b00e8987149209c1525408dab1
Sener Kaya <sener271@icloud.com>
2022-03-14 15:20:31 +0100

    article1-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { Article1Component } from './article1/article1.component';
+import { Article2Component } from './article2/article2.component';
+import { Article4Component } from './article4/article4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    Article1Component,
+    Article2Component,
+    Article4Component
   ],
   imports: [
     BrowserModule,
